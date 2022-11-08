ba87dcffd1cf725068e8463c693c8e24a5e39861
Camila Cerqueira <camila_cerqueira@sicredi.com.br>
2022-11-08 18:39:00 -0300

    #40 - Rotas React

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/navbar/Navbar';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Catalogo from "./components/catalogo/Catalogo";
+import Home from "./components/home/Home";
+import Cadastro from "./components/cadastro/Cadastro";
+import Login from "./components/login/Login";
+import Usuario from "./components/usuario/Usuario";
+import Anotacao from "./components/anotacao/Anotacao";
+import Ave from "./components/ave/Ave";
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/catalogo" element={<Catalogo />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/cadastro" element={<Cadastro />}></Route>
+          <Route path="/usuario" element={<Usuario />}></Route>
+          <Route path="/anotacao" element={<Anotacao />}></Route>
+          <Route path="/ave" element={<Ave />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ frontend/src/components/cadastro/Cadastro.tsx
@@ -0,0 +1,11 @@
+import "./Cadastro.css";
+
+const Cadastro = () => {
+  return (
+    <div>
+      <h1>Cadastro de usuário</h1>
+    </div>
+  );
+};
+
+export default Cadastro;
