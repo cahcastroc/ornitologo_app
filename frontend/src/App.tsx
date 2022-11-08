2e9f92ec67585d2107c8a95910216c1f5a8df8cf
André Abreu <andre_abreu@sicredi.com.br>
2022-11-08 15:59:20 -0300

    criaçao componente navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <></>
+    <Navbar/>
   );
 }
 
---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+
+  return (
+    <nav className='nav'>
+      
+        <ul >
+          <li>Home</li>
+          <li>Sobre</li>
+          <li>Catálogo</li>
+          <li>Login</li>
+        </ul>
+      
+    </nav>
+  )
+}
+
+export default Navbar
