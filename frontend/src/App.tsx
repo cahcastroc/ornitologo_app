fe1f051cad8ba35f1a82e1159d01d6eaeb60dbb0
André Abreu <andre_abreu@sicredi.com.br>
2022-11-09 13:17:29 -0300

    criação input generico e botao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Botao from "./components/botao/Botao";
+import Input from "./components/Input/Input";
+
 function App() {
   return (
     <>
-    <Navbar />
-    <Footer />
+      <Navbar />
+      <Botao />
+      <Footer />
+      <Input placeholder="senha" type="password"  />
     </>
-  )
+  );
 }
 
 export default App;
---
+++ frontend/src/components/botao/Botao.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Botao.css'
+
+
+const Botao = () => {
+    return (
+        <>
+            <a className='botao' href='/home'>CRIAR CONTA</a>    
+        </>
+  )
+}
+
+export default Botao
