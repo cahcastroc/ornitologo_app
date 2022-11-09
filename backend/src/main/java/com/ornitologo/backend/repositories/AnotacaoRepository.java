1be02581fa358fe92707fc5a5eaa510fe3523b84
Paulo Henrique <camargo_paulo@sicredi.com.br>
2022-11-09 17:08:37 -0300

    remove linhas nao usadas de Anotacaorepository

--- backend/src/main/java/com/ornitologo/backend/repositories/AnotacaoRepository.java
+++ backend/src/main/java/com/ornitologo/backend/repositories/AnotacaoRepository.java
@@ -3,7 +3,6 @@ package com.ornitologo.backend.repositories;
 import com.ornitologo.backend.entities.Anotacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
