a75419882756aa64b02523b74d2a04dc17ee6852
Paulo Henrique <camargo_paulo@sicredi.com.br>
2022-11-10 14:28:08 -0300

    cria componente de login

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -1,10 +1,23 @@
 import "./Login.css";
+import loginImage from "../../assets/login_cadastro.png"
+import Input from "../Input/Input";
+import Botao from "../botao/Botao";
 
 const Login = () => {
+  function onsubmit(){
+
+  }
+
   return (
-    <div>
-      <h1>Amogus</h1>
-    </div>
+    <main className="login-container">
+      <img src={loginImage}></img>
+      <form className="form-container">
+        <h1>Login</h1>
+        <Input placeholder="email" type="text"></Input>
+        <Input placeholder="senha" type="password"></Input>
+        <Botao text="login" enviar={() => onsubmit()} parametros={[]}></Botao>
+      </form>
+    </main>
   );
 };
 
