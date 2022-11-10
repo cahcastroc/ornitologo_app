65f5e6418e287e57dcf930fe2223534676c36b6b
André Abreu <andre_abreu@sicredi.com.br>
2022-11-10 15:32:44 -0300

    criação da pasta pages e cadastro de aves e reformulação da estrutura de páginas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Catalogo from "./components/catalogo/Catalogo";
-import Home from "./components/home/Home";
-import Cadastro from "./components/cadastro/Cadastro";
+import Catalogo from "./pages/catalogo/Catalogo";
+import Home from "./pages/home/Home";
+import Cadastro from "./pages/cadastro/Cadastro";
 import Login from "./components/login/Login";
 import Usuario from "./components/usuario/Usuario";
 import Anotacao from "./components/anotacao/Anotacao";
-import Ave from "./components/ave/Ave";
+import CadastroAve from "./pages/cadastroAve/CadastroAve";
 import Footer from "./components/footer/Footer";
 import Input from "./components/Input/Input";
 
@@ -26,7 +26,7 @@ function App() {
           <Route path="/cadastro" element={<Cadastro />}></Route>
           <Route path="/usuario" element={<Usuario />}></Route>
           <Route path="/anotacao" element={<Anotacao />}></Route>
-          <Route path="/ave" element={<Ave />}></Route>
+          <Route path="/cadastroAve" element={<CadastroAve />}></Route>
           <Route path='*' element={<h1>Página não encontrada!</h1>} />
         </Routes>
       </BrowserRouter>
