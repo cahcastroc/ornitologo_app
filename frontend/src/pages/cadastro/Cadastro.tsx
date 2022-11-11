7d773aadd60f1ca1b919aa64ff8a7543609351da
victorPeirgo <victorperigo@hotmail.com>
2022-11-11 18:21:52 -0300

    a

--- frontend/src/pages/cadastro/Cadastro.tsx
+++ frontend/src/pages/cadastro/Cadastro.tsx
@@ -1,37 +1,61 @@
-import cadastroImage from "../../assets/login_cadastro.png"
+import cadastroImage from "../../assets/login_cadastro.png";
 import Botao from "../../components/botao/Botao";
 import Input from "../../components/Input/Input";
 import { User } from "../../models/User";
 import { CadastroService } from "../../services/CadastroService";
 import "./Cadastro.css";
+import { useState } from "react";
 
 const Cadastro = () => {
-  let service: CadastroService = new CadastroService();
+    let service: CadastroService = new CadastroService();
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [confirmarSenha, setConfirmarSenha] = useState("");
 
-  function onsubmithandler(event: React.FormEvent){
-    event.preventDefault();
-    let user: User = {
-      nome: "amogus",
-      email: "sugoma",
-      senha: "sus"
+    function onsubmithandler(event: React.FormEvent) {
+        event.preventDefault();
+        let user: User = {
+            nome: nome,
+            email: email,
+            senha: senha,
+        };
+        service.postUser(user);
     }
-    service.postUser(user);
-  }
-  
-  return (
-    <main className="cadastro-container">
-      <img src={cadastroImage}></img>
-      <form className="form-container" onSubmit={(event) => onsubmithandler(event)}>
-        <h2>Cadastro</h2>
-        <Input placeholder="nome" type="text"></Input>
-        <Input placeholder="email" type="email"></Input>
-        <Input placeholder="senha" type="password"></Input>
-        <Input placeholder="confirmação da senha" type="password"></Input>
-        {/* <Botao text="cadastrar" enviar={() => onsubmit()} parametros={[]}></Botao> */}
-        <button type="submit">cadastra</button>
-      </form>
-    </main>
-  );
+
+    return (
+        <main className="cadastro-container">
+            <img src={cadastroImage}></img>
+            <form
+                className="form-container"
+                onSubmit={(event) => onsubmithandler(event)}
+            >
+                <h2>Cadastro</h2>
+                <Input
+                    placeholder="nome"
+                    type="text"
+                    onChange={setNome}
+                ></Input>
+                <Input
+                    placeholder="email"
+                    type="email"
+                    onChange={setEmail}
+                ></Input>
+                <Input
+                    placeholder="senha"
+                    type="password"
+                    onChange={setSenha}
+                ></Input>
+                <Input
+                    placeholder="confirmação da senha"
+                    type="password"
+                    onChange={setConfirmarSenha}
+                ></Input>
+                {/* <Botao text="cadastrar" enviar={() => onsubmit()} parametros={[]}></Botao> */}
+                <button type="submit">cadastra</button>
+            </form>
+        </main>
+    );
 };
 
 export default Cadastro;
--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,28 +1,42 @@
 import "./Login.css";
-import loginImage from "../../assets/login_cadastro.png"
+import loginImage from "../../assets/login_cadastro.png";
 import { Link } from "react-router-dom";
 import Input from "../../components/Input/Input";
 import Botao from "../../components/botao/Botao";
+import { useState } from "react";
 
 const Login = () => {
-  function onsubmit(){
+    function onsubmit() {}
 
-  }
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
 
-  return (
-    <main className="login-container">
-      <img src={loginImage}></img>
-      <form className="form-container">
-        <h2>Login</h2>
-        <Input placeholder="email" type="text"></Input>
-        <Input placeholder="senha" type="password"></Input>
-        <Link to="/cadastro">
-          <a href="">primeira vez? faça seu cadastro!</a>
-        </Link>
-        <Botao text="login" enviar={() => onsubmit()} parametros={[]}></Botao>
-      </form>
-    </main>
-  );
+    return (
+        <main className="login-container">
+            <img src={loginImage}></img>
+            <form className="form-container">
+                <h2>Login</h2>
+                <Input
+                    placeholder="email"
+                    type="text"
+                    onChange={setEmail}
+                ></Input>
+                <Input
+                    placeholder="email"
+                    type="email"
+                    onChange={setEmail}
+                ></Input>
+                <Link to="/cadastro">
+                    <a href="">primeira vez? faça seu cadastro!</a>
+                </Link>
+                <Botao
+                    text="login"
+                    enviar={() => onsubmit()}
+                    parametros={[]}
+                ></Botao>
+            </form>
+        </main>
+    );
 };
 
 export default Login;
