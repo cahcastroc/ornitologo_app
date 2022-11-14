663a796a6c652a1f03334b59572cc44e99a81e42
nicoli_simoes <nicolidss@gmail.com>
2022-11-14 17:17:28 -0300

    test controller anotação getAllByUser não funciona

--- backend/src/test/java/com/ornitologo/backend/controllersTest/AnotacaoControllerTest.java
+++ backend/src/test/java/com/ornitologo/backend/controllersTest/AnotacaoControllerTest.java
@@ -8,7 +8,9 @@ import com.ornitologo.backend.entities.Anotacao;
 import com.ornitologo.backend.entities.Ave;
 import com.ornitologo.backend.entities.Usuario;
 import com.ornitologo.backend.models.Localizacao;
+import com.ornitologo.backend.services.AnotacaoProducerService;
 import com.ornitologo.backend.services.AnotacaoService;
+import com.ornitologo.backend.services.UsuarioService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,11 +18,17 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -30,6 +38,7 @@ import java.security.interfaces.RSAKey;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,50 +47,66 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 @WebMvcTest(AnotacaoController.class)
 @Import(SecurityConfig.class)
 class AnotacaoControllerTest {
-    private AnotacaoDTO dto;
 
     @MockBean
     private AnotacaoService anotacaoService;
-
     @Autowired
     private MockMvc mockMvc;
-
+    private AnotacaoDTO dto;
+    private AnotacaoDTO dto2;
+    @MockBean
+    private AnotacaoProducerService anotacaoProducerService;
+    @MockBean
+    private UsuarioService  usuarioService;
 
 
     @BeforeEach
     public void setUp(){
-        Anotacao entityBody = new Anotacao(
-                1L,
-                null,
-                new Localizacao(),
-                "Comentário teste",
-                "Pequena",
-                "Azul",
-                Instant.now(),
-                null,
-                new Ave(),
-                new Usuario()
-        );
+        Anotacao entityBody =  Anotacao.builder()
+                        .id(1L)
+                        .comentario("Comentário teste")
+                        .tamanho("pequena")
+                        .corPredominante("azul")
+                .usuario(new Usuario(1L,"nicoli@nicoli.com.br",null,null,null,null))
+                        .build();
 
         dto = new AnotacaoDTO(entityBody);
+
+        Anotacao entityBody2 =Anotacao.builder()
+                .id(2L)
+                .comentario("Comentário teste 2")
+                .tamanho("grande")
+                .corPredominante("roxa")
+                .usuario(new Usuario(1L,"nicoli@nicoli.com.br",null,null,null,null))
+                .build();
+
+        dto2 = new AnotacaoDTO(entityBody2);
     }
 
 
 
     @Test
-    @WithMockUser(value = "user")
+    @WithMockUser()
     void deveRetornarTodasAsAnotacoes() throws Exception{
+
+        Jwt jwt = Jwt.withTokenValue("token")
+                .header("alg", "none")
+                .claim("sub", "user")
+                .build();
+        Collection<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("SCOPE_read");
+        JwtAuthenticationToken token = new JwtAuthenticationToken(jwt, authorities);
+
         List<AnotacaoDTO> lista = new ArrayList<AnotacaoDTO>();
         lista.add(dto);
+        lista.add(dto2);
 //        List<AnotacaoDTO> lista = Arrays.asList(
 //                AnotacaoDTO.builder()
 //                        .id(1L)
-//                        .dataHorarioDoAvistamento(null)
 //                        .localizacao(null)
 //                        .comentario("Comentário teste")
 //                        .tamanho("pequena")
 //                        .corPredominante("azul")
-//                        .build(),
+//                        .build());
 //                AnotacaoDTO.builder()
 //                        .id(2L)
 //                        .dataHorarioDoAvistamento(null)
@@ -91,22 +116,18 @@ class AnotacaoControllerTest {
 //                        .corPredominante("roxa")
 //                        .build()
 //        );
-        Mockito.when(anotacaoService.getAllByUser(jwt().toString())).thenReturn(lista);
+        Mockito.when(anotacaoService.getAllByUser("token")).thenReturn(lista);
 
-        mockMvc.perform(get("/anotacoes")
-                .with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_messages"))))
-                .andExpectAll(jsonPath("$.[0].id").value("1L"))
-                .andExpectAll(jsonPath("$.[0].dataHorarioDoAvistamento").value(null))
-                .andExpectAll(jsonPath("$.[0].localizacao").value(null))
+        mockMvc.perform(get("/anotacoes/1").with(authentication(token)))
+                .andExpect(status().isOk())
+                .andExpectAll(jsonPath("$.[0].id").value(1L))
                 .andExpectAll(jsonPath("$.[0].comentario").value("Comentário teste"))
                 .andExpectAll(jsonPath("$.[0].tamanho").value("pequena"))
-                .andExpectAll(jsonPath("$.[0].cor").value("azul"));
-//                .andExpectAll(jsonPath("$.[1].id").value("2L"))
-//                .andExpectAll(jsonPath("$.[1].dataHorarioDoAvistamento").value(null))
-//                .andExpectAll(jsonPath("$.[1].localizacao").value(null))
-//                .andExpectAll(jsonPath("$.[1].comentario").value("Comentário teste 2"))
-//                .andExpectAll(jsonPath("$.[1].tamanho").value("grande"))
-//                .andExpectAll(jsonPath("$.[1].cor").value("roxa"));
+                .andExpectAll(jsonPath("$.[0].cor").value("azul"))
+                .andExpectAll(jsonPath("$.[1].id").value("2L"))
+                .andExpectAll(jsonPath("$.[1].comentario").value("Comentário teste 2"))
+                .andExpectAll(jsonPath("$.[1].tamanho").value("grande"))
+                .andExpectAll(jsonPath("$.[1].cor").value("roxa"));
 
             }
 
