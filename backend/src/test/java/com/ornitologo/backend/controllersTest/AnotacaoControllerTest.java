1ba267fe741f3e53301f6b46cb5f1a352bbf288f
nicoli_simoes <nicolidss@gmail.com>
2022-11-15 20:24:24 -0300

    test controller anotação getById funcionando

--- backend/src/test/java/com/ornitologo/backend/controllersTest/AnotacaoControllerTest.java
+++ backend/src/test/java/com/ornitologo/backend/controllersTest/AnotacaoControllerTest.java
@@ -35,8 +35,6 @@ import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import java.security.interfaces.RSAKey;
 import java.time.Instant;
@@ -47,9 +45,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(AnotacaoController.class)
+@WebMvcTest(controllers = AnotacaoController.class)
 @Import(SecurityConfig.class)
 class AnotacaoControllerTest {
 
@@ -92,51 +91,35 @@ class AnotacaoControllerTest {
 
 
 
-    @Test
-    @WithMockUser
-    void deveRetornarTodasAsAnotacoes() throws Exception{
-
-        Jwt jwt = Jwt.withTokenValue("token")
-                .header("alg", "none")
-                .claim("sub", "user")
-                .build();
-        Collection<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("SCOPE_read");
-        JwtAuthenticationToken token = new JwtAuthenticationToken(jwt, authorities);
-
-        List<AnotacaoDTO> lista = new ArrayList<AnotacaoDTO>();
-        lista.add(dto);
-        lista.add(dto2);
-//        List<AnotacaoDTO> lista = Arrays.asList(
-//                AnotacaoDTO.builder()
-//                        .id(1L)
-//                        .localizacao(null)
-//                        .comentario("Comentário teste")
-//                        .tamanho("pequena")
-//                        .corPredominante("azul")
-//                        .build());
-//                AnotacaoDTO.builder()
-//                        .id(2L)
-//                        .dataHorarioDoAvistamento(null)
-//                        .localizacao(null)
-//                        .comentario("Comentário teste 2")
-//                        .tamanho("grande")
-//                        .corPredominante("roxa")
-//                        .build()
-//        );
-        Mockito.when(anotacaoService.getAllByUser("token")).thenReturn(lista);
-
-        mockMvc.perform(get("/anotacoes/").header("Authorization", token))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpectAll(jsonPath("$.[0].id").value(1L))
-                .andExpectAll(jsonPath("$.[0].comentario").value("Comentário teste"))
-                .andExpectAll(jsonPath("$.[0].tamanho").value("pequena"))
-                .andExpectAll(jsonPath("$.[0].cor").value("azul"))
-                .andExpectAll(jsonPath("$.[1].id").value("2L"))
-                .andExpectAll(jsonPath("$.[1].comentario").value("Comentário teste 2"))
-                .andExpectAll(jsonPath("$.[1].tamanho").value("grande"))
-                .andExpectAll(jsonPath("$.[1].cor").value("roxa"));
-
-            }
+//    @Test
+//    @WithMockUser
+//    void deveRetornarTodasAsAnotacoes() throws Exception{
+//
+//        Jwt jwt = Jwt.withTokenValue("token")
+//                .header("alg", "none")
+//                .claim("sub", "user")
+//                .build();
+//        Collection<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("SCOPE_read");
+//        JwtAuthenticationToken token = new JwtAuthenticationToken(jwt, authorities);
+//
+//        List<AnotacaoDTO> lista = new ArrayList<AnotacaoDTO>();
+//        lista.add(dto);
+//        lista.add(dto2);
+//
+//        Mockito.when(anotacaoService.getAllByUser("token")).thenReturn(lista);
+//
+//        mockMvc.perform(get("/anotacoes/").header("Authorization", token))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpectAll(jsonPath("$.[0].id").value(1L))
+//                .andExpectAll(jsonPath("$.[0].comentario").value("Comentário teste"))
+//                .andExpectAll(jsonPath("$.[0].tamanho").value("pequena"))
+//                .andExpectAll(jsonPath("$.[0].cor").value("azul"))
+//                .andExpectAll(jsonPath("$.[1].id").value("2L"))
+//                .andExpectAll(jsonPath("$.[1].comentario").value("Comentário teste 2"))
+//                .andExpectAll(jsonPath("$.[1].tamanho").value("grande"))
+//                .andExpectAll(jsonPath("$.[1].cor").value("roxa"));
+//
+//            }
 
     @Test
     @WithMockUser
@@ -155,37 +138,49 @@ class AnotacaoControllerTest {
                 .andExpectAll(jsonPath("$.corPredominante").value("azul"));
     }
 
+//    @Test
+//    @WithMockUser
+//    void create() throws Exception {
+//
+//       var json= new Gson().toJson(dto2);
+//
+//       Mockito.when(anotacaoService.create(dto2)).thenReturn();
+//
+//       mockMvc.perform(post("/anotacoes").contentType(MediaType.APPLICATION_JSON)
+//               .accept(MediaType.APPLICATION_JSON)
+//               .content(json))
+//               .andExpect(MockMvcResultMatchers.status().isOk()
+//                       .andExpectAll(jsonPath("$.id").value("2"))
+//                       .andExpectAll(jsonPath("$.comentario").value("Comentário teste2"))
+//                       .andExpectAll(jsonPath("$.tamanho").value("grande"))
+//                       .andExpectAll(jsonPath("$.corPredominante").value("roxo")));
+//
+//
+//
+//    }
+
     @Test
     @WithMockUser
-    void create() throws Exception {
-//        Anotacao entityBody3 =  Anotacao.builder()
-//                .id(3L)
-//                .comentario("Adição de anotacao")
-//                .tamanho("pequena")
-//                .corPredominante("azul")
-//                .usuario(new Usuario(1L,"nicoli@nicoli.com.br",null,null,null,null))
+    void shouldBeUpdateAndReturn200() throws Exception{
+//        AnotacaoDTO dto;
+//        Anotacao entityBody =  Anotacao.builder()
+//                .id(1L)
+//                .comentario("Comentário teste")
+//                .tamanho("Grande")
+//                .corPredominante("vermelha")
 //                .build();
-//
-//       AnotacaoDTO dto3 = new AnotacaoDTO(entityBody3);
-       var json= new Gson().toJson(dto2);
+//        dto = new AnotacaoDTO(entityBody);
 
-       Mockito.when(anotacaoService.create(dto2)).thenReturn();
+        Mockito.when(anotacaoService.update(1L,dto)).thenReturn(dto2);
 
-       mockMvc.perform(post("/anotacoes").contentType(MediaType.APPLICATION_JSON)
-               .accept(MediaType.APPLICATION_JSON)
-               .content(json))
-               .andExpect(MockMvcResultMatchers.status().isOk()
-                       .andExpectAll(jsonPath("$.id").value("2"))
-                       .andExpectAll(jsonPath("$.comentario").value("Comentário teste2"))
-                       .andExpectAll(jsonPath("$.tamanho").value("grande"))
-                       .andExpectAll(jsonPath("$.corPredominante").value("roxo"));
+        var json= new Gson().toJson(dto);
 
-
-
-    }
-
-    @Test
-    void update() {
+        mockMvc.perform(put("/anotacoes/{id}","1").contentType(MediaType.APPLICATION_JSON).content(json))
+                        .andExpect(MockMvcResultMatchers.status().isOk())
+                        .andExpectAll(jsonPath("$.id").value("1L"))
+                        .andExpectAll(jsonPath("$.comentario").value("Comentário teste2"))
+                        .andExpectAll(jsonPath("$.tamanho").value("grande"))
+                        .andExpectAll(jsonPath("$.corPredominante").value("roxo"));
     }
 
     @Test
