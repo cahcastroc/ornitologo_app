e6fe63563bbbe53f3973a26604d4b01ff812a42f
Paulo Henrique <camargo_paulo@sicredi.com.br>
2022-11-17 12:11:48 -0300

    adiciona lista de aves dinamica no select

--- frontend/src/pages/anotacoesUsuario/AnotacoesUsuario.tsx
+++ frontend/src/pages/anotacoesUsuario/AnotacoesUsuario.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import binoculo from "../../assets/binoculo.png";
 import Input from "../../components/Input/Input";
 import IAve from "../../interfaces/IAve";
+import BotaoSalvar from "../../components/botaoSalvar/BotaoSalvar";
 import { useState, useEffect } from "react";
 import { AnotacaoService } from "./AnotacaoService";
-import BotaoSalvar from "../../components/botaoSalvar/BotaoSalvar";
 import { Anotacao } from "../../models/Anotacao";
 import { ILocalizacao } from "../../interfaces/ILocalizacao";
 import { Localizacao } from "../../models/Localizacao";
+import { useNavigate } from "react-router-dom";
+import { CatalogoService } from "../catalogo/CatalogoService";
 
 const NovaAnotacao = () => {
   let user = JSON.parse(localStorage.getItem("token") || "{}");
@@ -18,33 +20,45 @@ const NovaAnotacao = () => {
   let [latitude, setLatitude] = useState("");
   let [longitude, setLongitude] = useState("");
   let [comentario, setComentario] = useState("");
-  let [ave, setAve] = useState<IAve>({id:1, descricao: "Um passaro", nomeCientifico: "Barro de Joao", nomePopular: "João de Barro"});
-  let options: IAve[] = [];
+  let [options, setOptions] = useState<IAve[]>([]);
+  let [aveId, setAveId] = useState<number>(0);
+  
+  const navigate = useNavigate();
   const service: AnotacaoService = new AnotacaoService();
+  const optionsService: CatalogoService = new CatalogoService();
 
   useEffect(()=>{
-  })
+    optionsService.getAves().then((arr)=> setOptions(arr));}
+  )
 
   function onSubmitHandler(event: React.FormEvent){
-      event.preventDefault();
-      let localizacao: ILocalizacao = new Localizacao({
-        lat: latitude,
-        longt: longitude,
-        descricao: "amogus"
-      });
+    event.preventDefault();
+    let localizacao: ILocalizacao = new Localizacao({
+      lat: latitude,
+      longt: longitude,
+      descricao: ""
+    });
 
-      let body: Anotacao = new Anotacao({
-        ave: ave,
-        comentario: comentario,
-        corPredominante: cor,
-        localizacao: localizacao,
-        tamanho: tamanho,
-        usuarioId: user.id,
-        id: 0,
-        dataHorarioDoAvistamento: "",
-        criadoEm: ""
-      });
-      service.create(body)
+    let body: Anotacao = new Anotacao({
+      ave: {
+        id: aveId
+      },
+      comentario: comentario,
+      corPredominante: cor,
+      localizacao: localizacao,
+      tamanho: tamanho,
+      usuarioId: user.id,
+      id: 0,
+      dataHorarioDoAvistamento: "",
+      criadoEm: ""
+    });
+    service.create(body).then(()=>{
+      navigate("/minhasanotacoes");
+    });
+  }
+
+  function handleSelectChange(e: any){
+    setAveId(e.target.value);
   }
    
   return (
@@ -53,8 +67,10 @@ const NovaAnotacao = () => {
       <h2>Nova anotação</h2>
       <form  className="nova-anotacao-form" onSubmit={(event) => onSubmitHandler(event)}>
         <div className="inputs-container">
-          <select name="" id="">
-            <option value="id">Joao de Barro</option>
+          <select onChange={handleSelectChange}>
+            {options.map((item) =>{
+              return <option key={item.id} value={item.id}>{item.nomePopular}</option>
+            })} 
           </select>
           <Input placeholder="cor" type="text" onChange={setCor}></Input>
           <Input placeholder="tamanho" type="text" onChange={setTamanho}></Input>
