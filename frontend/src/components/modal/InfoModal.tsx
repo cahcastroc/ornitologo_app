714d233ecbcf16377ba4540100245671dec7dfc6
Camila Cerqueira <camila_cerqueira@sicredi.com.br>
2022-11-17 21:23:34 -0300

    fix-txt-home-modal

--- frontend/src/components/modal/InfoModal.tsx
+++ frontend/src/components/modal/InfoModal.tsx
@@ -1,25 +1,20 @@
 import LocationIcon from "@mui/icons-material/LocationOnRounded";
-import DataIcon from "@mui/icons-material/CalendarMonthRounded";
 import "./Modal.css";
 import IAnotacao from "../../interfaces/IAnotacao";
 import DescricraoIcon from "@mui/icons-material/AssignmentOutlined";
 
 interface Props {
-  anotacao: IAnotacao; 
+  anotacao: IAnotacao;
 }
 
-const InfoModal = ({ anotacao}: Props) => {
-  const { ave, comentario, dataHorarioDoAvistamento, localizacao } = anotacao;
+const InfoModal = ({ anotacao }: Props) => {
+  const { ave, comentario, localizacao } = anotacao;
 
   return (
     <div className="info-modal">
       <h2>{ave.nomePopular}</h2>
       <h3>{ave.nomeCientifico}</h3>
       <p>{comentario}</p>
-      <div className="data">
-        <DataIcon />
-        <h4>{dataHorarioDoAvistamento || ""}</h4>
-      </div>
       <div className="latitude">
         <LocationIcon />
         <h4>{localizacao.lat || ""}</h4>
